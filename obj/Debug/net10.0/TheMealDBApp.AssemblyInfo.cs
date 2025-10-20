73f612a5018d4f7a2f7f98c0fb976b532b7d4985
tifani3112 <102417496+tifani3112@users.noreply.github.com>
2025-10-20 09:02:55 +0700

    minor update

--- obj/Debug/net10.0/TheMealDBApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/TheMealDBApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheMealDBApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e7f04c21c1b716b9dc72773a3a6e2c9913c2b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f357b1a9d910e4597fce3b4f57d37e26f66ef62")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheMealDBApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheMealDBApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
